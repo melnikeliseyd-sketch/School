a6c04bdf967db893f8750d787270b65d62d3e1f0
TheWasab1 <melnikeliseyd@gmail.com>
2025-11-12 15:17:48 +0200

    Версия 1.1 добавлена функция удаления задач.

--- 5.1.py
+++ 5.1.py
@@ -1,7 +1,7 @@
 # 5.1 дз
 tasks = []
 
-
+#показать задачи
 def show_tasks():
     if not tasks:
         print("Список пуст.")
@@ -9,19 +9,28 @@ def show_tasks():
         for i, task in enumerate(tasks, 1):
             print(f"{i}. {task}")
 
-
+# добавить задачу
 def add_task(task):
     tasks.append(task)
     print(f"Добавлено: {task}")
-
+#удалить задачу
+def delete_task(index):
+    try:
+        removed = tasks.pop(index - 1)
+        print(f"Удалено: {removed}")
+    except IndexError:
+        print("Нет задачи с таким номером.")
 
 while True:
-    command = input("\nВведите команду (add/show/exit): ").strip()
+    command = input("\nВведите команду (add/delete/show/exit/): ").strip()
 
     if command == "add":
         task = input("Введите задачу: ")
         add_task(task)
     elif command == "show":
         show_tasks()
+    elif command == "delete":
+        num = int(input("Введите номер задачи для удаления: "))
+        delete_task(num)
     elif command == "exit":
         break
